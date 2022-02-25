a4fa13a159bc7bcff0439b093fe82bf6b9b606a4
Norris Luong <33255034+Gsirron@users.noreply.github.com>
2022-02-25 22:34:17 +1100

    chore: made planetgroup to have indipendent mesh

--- src/components/canvas/Planet/PlanetGroup.jsx
+++ src/components/canvas/Planet/PlanetGroup.jsx
@@ -14,18 +14,41 @@ const PlanetGroup = () => {
   const groupRef = useRef()
   const ringRef1 = useRef()
   const ringRef2 = useRef()
+  const ring = new THREE.TorusBufferGeometry(3.5, 0.3, 3, 30)
+  const ringColor = new THREE.MeshBasicMaterial({ color: 'orange' })
   const obj = new THREE.Object3D()
 
   useFrame(({ clock }) => {
-    const t = Math.sin(clock.getElapsedTime)
+    const t = Math.sin(clock.getElapsedTime())
+
+    ringRef1.current.position.y = t + 1
   })
 
   return (
     <group ref={groupRef} position={[0, 0, 0]}>
-      <PlanetRings rad={3.2} position={[0, 1, 0]} />
+      <mesh
+        ref={ringRef1}
+        rotation={[Math.PI / 2, 0, 0]}
+        position={[0, 1, 0]}
+        geometry={ring}
+        material={ringColor}
+      />
+      <mesh
+        ref={ringRef2}
+        rotation={[Math.PI / 2, 0, 0]}
+        position={[0, -1, 0]}
+        geometry={ring}
+        material={ringColor}
+      />
+      <mesh
+        rotation={[Math.PI / 2, 0, 0]}
+        geometry={ring}
+        material={ringColor}
+      />
+      {/* <PlanetRings rad={3.2} position={[0, 1, 0]} />
       <PlanetRings rad={3.2} position={[0, -1, 0]} />
       <PlanetRings rad={3.7} />
-      {/* <PlanetRings /> */}
+      <PlanetRings /> */}
       <Planet />
     </group>
   )
